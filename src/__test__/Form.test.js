c420d831998be7d8f6567d721b15440b5d73c79b
vianey-jean <vianey.jean@ymail.com>
2023-02-08 11:39:26 +0400

    Finalisation du test

--- src/__test__/Form.test.js
+++ src/__test__/Form.test.js
@@ -1,30 +1,22 @@
-import { render, screen } from '@testing-library/react'
-import { Provider } from 'react-redux'
-import Form from '../components/Form/Form';
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import Form from "../components/Form/Form";
 
-import { store } from '../redux/store'
+import { store } from "../redux/store";
 
+describe("Should be display in create employee form", () => {
+  test("it should have inputs", async () => {
+    render(
+      <Provider store={store}>
+        <Form />
+      </Provider>
+    );
 
-describe('Should be display in create employee form',  () => {
-    test('it should have inputs', async () => {
-        render(
-            <Provider store={store}>
-                 <Form/>
-            </Provider>
-        );
+    expect(screen.getAllByRole("textbox")).toBeTruthy();
 
-        expect(
-            screen.getAllByRole('textbox')
-        ).toBeTruthy()
-       
-        expect(
-            screen.getByLabelText('Date of Birth')
-        ).toBeTruthy()
-        expect(
-            screen.getByLabelText('Start Date')
-        ).toBeTruthy()
-        expect(
-            screen.getByRole('button')
-        ).toBeTruthy()
-    })
-})
+    expect(screen.getByLabelText("Date of Birth")).toBeTruthy();
+    expect(screen.getByLabelText("Start Date")).toBeTruthy();
+    expect(screen.getByLabelText("City")).toBeTruthy();
+    expect(screen.getByRole("button")).toBeTruthy();
+  });
+});
