559cb38f301090e4510d9a5f7c76533757c26dd5
vianey-jean <vianey.jean@ymail.com>
2023-02-10 20:47:03 +0400

    Mise a jour code mock

--- src/Moks/departements.js
+++ src/Moks/departements.js
@@ -2,10 +2,6 @@ export const listesDepartement = [
   { value: "sales", label: "Sales", name: "departament" },
   { value: "marketing", label: "Marketing", name: "departament" },
   { value: "engeneering", label: "Engeneering", name: "departament" },
-  {
-    value: "humanresources",
-    label: "Humanresources",
-    name: "departament",
-  },
+  { value: "humanresources", label: "Humanresources", name: "departament" },
   { value: "legal", label: "Legal", name: "departament" },
 ];
